ae1e061ea8b04d22298296302e562fab8e8ccdd6
ct10061006 <charles.tang@rutgers.edu>
2026-03-24 23:15:03 -0400

    Fixed motor encoder information

--- src/run_tensegrity_hybrid_mppi.py
+++ src/run_tensegrity_hybrid_mppi.py
@@ -327,11 +327,11 @@ class TensegrityRobot:
             motor.speed = float(self.speed[motor_id])
             motor.done = bool(self.done[motor_id]) if self.done is not None else False
             motor.encoder_counts = int(self.encoder_counts[motor_id])
-            '''if(motor.id % 2 == 1):
-                motor.encoder_length = 180 + self.encoder_length[motor_id]# NEW
+            if(motor.id % 2 == 1):
+                motor.encoder_length = float(self.encoder_length[motor_id])# NEW
             else:
-                motor.encoder_length = 180 - self.encoder_length[motor_id]'''
-            motor.encoder_length = float(self.encoder_length[motor_id])
+                motor.encoder_length = float(-self.encoder_length[motor_id])'''
+            #motor.encoder_length = float(self.encoder_length[motor_id])
             control_msg.motors.append(motor)
 
         for sensor_id in range(self.num_sensors):
