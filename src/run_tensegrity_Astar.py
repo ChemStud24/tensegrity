9493ff973eabc354397ca7fcd1b686502e846f57
ct10061006 <charles.tang@rutgers.edu>
2026-07-16 17:39:40 -0400

    Added encoder length reporting

--- src/run_tensegrity_Astar.py
+++ src/run_tensegrity_Astar.py
@@ -49,6 +49,7 @@ class TensegrityRobot:
         self.gyroscope = [[0]*3 for _ in range(3)]
         self.encoder_counts = [0]*self.num_motors
         self.encoder_length = [0]*self.num_motors
+        self.absolute_encoder_length = [0]*self.num_motors
         self.RANGE024 = 100
         self.RANGE135 = 100
         self.max_speed = 70
@@ -242,11 +243,11 @@ class TensegrityRobot:
            # motor.direction = command[motor_id] > 0
            motor.done = self.done[motor_id]
            motor.encoder_counts = int(self.encoder_counts[motor_id])
-           if(motor.id < 3):
-               motor.encoder_length = 180 + self.encoder_length[motor_id]# NEW
+           motor.encoder_length = float(self.encoder_length[motor_id])
+           if(motor.id % 2 == 1):
+               motor.absolute_encoder_length = 180 + float(self.encoder_length[motor_id])
            else:
-               motor.encoder_length = 180 - self.encoder_length[motor_id]
-           #motor.encoder_length = self.encoder_length[motor_id]
+               motor.absolute_encoder_length = 180 - float(self.encoder_length[motor_id])
            control_msg.motors.append(motor)
         # sensors
         for sensor_id in range(self.num_sensors):
